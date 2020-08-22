8884ccafc89284193a34298f9120dff9573985a1
abhijit shaw <36149425+shaw2018@users.noreply.github.com>
2020-08-22 22:40:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -155,23 +155,4 @@ client.on('message', message => {
 });
 
 
-
-// voice stuff (Still a W.I.P)
-
-client.on('message', message => {
-  if (!message.guild) return;
-
-  if (message.content === 'a.join') {
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { 
-          message.reply('I have successfully connected to the channel!');
-        })
-        .catch(console.log);
-    } else {
-      message.reply('You need to join a voice channel first!');
-  }
-  }
-});
-
 client.login('NzQ2NjkxOTU5NzA3NTMzMzc0.X0EBFA.rvnlEQOJhuAUk-UW75dzk2tzark');
